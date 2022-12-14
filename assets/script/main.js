9c3c1b70411938f9ea229b2b92165a814d2fe83d
youssra <112857270+zombie0youssra@users.noreply.github.com>
2022-12-14 11:40:02 +0100

    shake shake seniora

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,28 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', colorchange )
+
+function colorchange(){
+  frontend.classList.toggle('colorpop')
+}
+
+let design = document.querySelector('a:nth-of-type(2)')
+
+design.addEventListener('click',gradientcolor)
+
+function gradientcolor(){
+  design.classList.toggle('bordergradient')
+}
+
+let and = document.querySelector('a:nth-of-type(3)')
+
+and.addEventListener('click', shakebutton)
+and.addEventListener('animationend', shakebutton)
+
+function shakebutton() {
+  and.classList.toggle('shake')
+}

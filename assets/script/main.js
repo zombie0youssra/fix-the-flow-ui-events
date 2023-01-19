d35f932516243c437bfa04ee2bd8d901ec1737ff
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-01-19 09:28:18 +0100

    j

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,50 +1,58 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+let interaction = document.querySelector("a:nth-of-type(12)");
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+interaction.addEventListener("click", jumpHandler);
+interaction.addEventListener("animationend", jumpHandler);
 
 function jumpHandler() {
-  interaction.classList.toggle('jump')
+  interaction.classList.toggle("jump");
 }
 
-let frontend = document.querySelector('a:nth-of-type(1)')
+let frontend = document.querySelector("a:nth-of-type(1)");
 
-frontend.addEventListener('click', colorchange )
+frontend.addEventListener("click", colorchange);
 
-function colorchange(){
-  frontend.classList.toggle('colorpop')
+function colorchange() {
+  frontend.classList.toggle("colorpop");
 }
 
-let design = document.querySelector('a:nth-of-type(2)')
+let design = document.querySelector("a:nth-of-type(2)");
 
-design.addEventListener('click',gradientcolor)
-design.addEventListener('animationend', gradientcolor)
+design.addEventListener("click", gradientcolor);
+design.addEventListener("animationend", gradientcolor);
 
-function gradientcolor(){
-  design.classList.toggle('bordergradient')
+function gradientcolor() {
+  design.classList.toggle("bordergradient");
 }
 
-let and = document.querySelector('a:nth-of-type(3)')
+let and = document.querySelector("a:nth-of-type(3)");
 
-and.addEventListener('click', shakebutton)
-and.addEventListener('animationend', shakebutton)
+and.addEventListener("click", shakebutton);
+and.addEventListener("animationend", shakebutton);
 
 function shakebutton() {
-  and.classList.toggle('shake')
+  and.classList.toggle("shake");
 }
 
-let development = document.querySelector('a:nth-of-type(4)')
+let development = document.querySelector("a:nth-of-type(4)");
 
-development.addEventListener('click', buttonfade)
+development.addEventListener("click", buttonfade);
 
-function buttonfade(){
-  development.classList.toggle('btn-4')
+function buttonfade() {
+  development.classList.toggle("btn-4");
 }
 
-let sprint = document.querySelector('a:nth-of-type(4)')
+let sprint = document.querySelector("a:nth-of-type(4)");
 
-sprint.addEventListener('click', buttonfade)
+sprint.addEventListener("click", buttonfade);
 
-function buttonfade(){
-  development.classList.toggle('btn-4')
+function buttonfade() {
+  sprint.classList.toggle("btn-4");
+}
+
+let fix = document.querySelector("a:nth-of-type(5)");
+
+fix.addEventListener("click", buttonfade);
+
+function buttonfade() {
+  fix.classList.toggle("btn-5");
 }

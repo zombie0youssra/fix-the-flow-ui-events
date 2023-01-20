7b1c74d423ed94c655b80835ed03186893394e0a
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-01-20 09:42:09 +0100

    script

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,3 +22,35 @@ design.addEventListener("click", grow);
 function grow() {
   design.classList.toggle("btn-2");
 }
+
+let and = document.querySelector("a:nth-of-type(3)");
+
+and.addEventListener("click", wiggle);
+
+function wiggle() {
+  and.classList.toggle("btn-3");
+}
+
+let development = document.querySelector("a:nth-of-type(4)");
+
+development.addEventListener("click", lightsout);
+
+function lightsout() {
+  development.classList.toggle("lightsout");
+}
+
+let sprint = document.querySelector("a:nth-of-type(5)");
+
+sprint.addEventListener("click", rainbow);
+
+function rainbow() {
+  sprint.classList.toggle("btn-5");
+}
+
+let fix = document.querySelector("a:nth-of-type(6)");
+
+fix.addEventListener("click", bounce);
+
+function bounce() {
+  fix.classList.toggle("btn-6");
+}
